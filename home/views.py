89a3cb239a3bba8a87b1848b08eb10ac27e3c155
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-07-27 10:40:39 +0530

    updating...

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
+from __future__ import unicode_literals
 
 # Create your views here.
 def index(request):
-    pass
+   return render(request,'index.html')
 def signup(request):
     pass
 def login(request):
