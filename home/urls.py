0fbf39eeebd8e6dcc715854aba0ae4c6f3cd13ad
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-08-15 22:17:53 +0530

    final touch started

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,9 @@ from django.urls import path,include
 from .views import sudoku, rank, changeRankType, saveQuestion, loginUser
 app_name="home"
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('',sudoku)
-    path('sudoku/', sudoku),
-    path('sudoku/loginUser/', loginUser),
+    path('',sudoku),
+    path('sudoku/',sudoku),
+    path('sudoku/loginUser/',loginUser),
     path('sudoku/rank/', rank),
     path('changeRankType/', changeRankType),
     path('sudoku/saveQuestion/', saveQuestion)
